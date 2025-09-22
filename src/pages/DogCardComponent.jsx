71e2dc2bc1ae5c357f14516874c62b90b70952c1
Kseniia <anillinnittakorall@gmail.com>
2025-09-23 01:29:09 +0200

    Add DogCardComponent for displaying dog details and update Dogs page to use it

---
+++ src/pages/DogCardComponent.jsx
@@ -0,0 +1,53 @@
+import { useState, useEffect } from "react"
+import { useNavigate } from "react-router"
+import './Dogs.css'
+
+function DogCardComponent({dog, onMoreInfo}) {
+    const [isRealImage, setIsRealImage] = useState(true)
+
+
+    function handleLoad(e) {
+        if (e.target.naturalHeight <= 1 || e.target.naturalWidth <= 1) {
+            setIsRealImage(false);
+        }
+    }
+
+
+    return (
+        
+            <section className="dog-card">
+
+                <li key={dog.id}>
+
+                    <div
+                        className="dog-background"
+                        style={isRealImage ? { backgroundImage: `url(${dog.img})` } : {}}
+                    >
+                        <img
+                            src={dog.img}
+                            alt=""
+                            style={{ display: "none" }}
+                            onError={() => setIsRealImage(false)}
+                            onLoad={handleLoad}
+                        />
+                        {!isRealImage && (
+                            <div id="missing-img">
+                                <p>Photo of {dog.name} will be here soon!</p>
+                            </div>
+                        )}
+                    </div>
+
+
+                    <h2>{dog.name}</h2>
+                    <p>Age: {dog.age}</p>
+                    <p>Breed: {dog.breed}</p>
+                    <div id="more-info-button">
+                        <button onClick={() => onMoreInfo(dog)}>More info</button>
+                    </div>
+                </li>
+            </section>
+        
+    )
+
+}
+export default DogCardComponent
--- src/pages/Dogs.jsx
+++ src/pages/Dogs.jsx
@@ -1,65 +1,83 @@
 import { useState, useEffect } from "react"
 import { useNavigate } from "react-router"
 import './Dogs.css'
-
+import './DogCardComponent.jsx'
+import DogCardComponent from "./DogCardComponent.jsx";
 
 
 function Dogs() {
     const XMasterKey = import.meta.env.VITE_JSONBIN_KEY;
-//const XMasterKey = "$2a$10$3EFI2pdzfX7xpZkYBCjOrewUUWdup6WeJS.YpPKB4RBpouvjO90pS"
-const [dogsList, setDogsList] = useState([]) 
-const [loading, setLoading] = useState(true)
-const [error, setError] = useState(null)
+    //const XMasterKey = "$2a$10$3EFI2pdzfX7xpZkYBCjOrewUUWdup6WeJS.YpPKB4RBpouvjO90pS"
+    const [dogsList, setDogsList] = useState([])
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState(null)
 
-const navigate = useNavigate();
+    const navigate = useNavigate();
 
 
-useEffect(() => {
-    fetch("https://api.jsonbin.io/v3/b/68cd832fae596e708ff41859", {
-        headers: {
-            'X-Master-Key': XMasterKey
-        }
+
+
+    useEffect(() => {
+        fetch("https://api.jsonbin.io/v3/b/68cd832fae596e708ff41859", {
+            headers: {
+                'X-Master-Key': XMasterKey
+            }
         })
-        .then(res => res.json())
-        .then(data => {
-            console.log(data)
-            setDogsList(data.record.record)
-            setLoading(false)
-        })
-        .catch(err => {
-            setError(err.message)
-            setLoading(false)
-        })
-}, [])
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                setDogsList(data.record.record)
+                setLoading(false)
+            })
+            .catch(err => {
+                setError(err.message)
+                setLoading(false)
+            })
+    }, [])
 
-if (loading) return <p>Loading...</p>
-if (error) return <p>Error: {error}</p>
-function NavigateToDogInfo(dog) {
-    navigate('/dog_info', {state: {dog}})
-    
-}
+    if (loading) return <p id="loading">Loading...</p>
+    if (error) return <p>Error: {error}</p>
 
 
-  return (
-   <>
-       <h1>Our Dogs</h1>
-       <ul>
-        {dogsList.map(dog => (
-            //check that each dog has a unique id
-            <section className="dog-card">
-            
-            <li key ={dog.id}>
-                <img src={dog.img} alt={`Photo of ${dog.name} coming soon`} className="dog-image"/>
-                <h2>{dog.name}</h2>
-                <p>Age: {dog.age}</p>
-                <p>Breed: {dog.breed}</p>
-            <button onClick={() => NavigateToDogInfo(dog)}>More info</button>
-            </li>
-            </section>
-        ))}
-       </ul>       
-   </>
-  )
+
+    function NavigateToDogInfo(dog) {
+        navigate('/dog_info', { state: { dog } })
+
+    }
+
+
+    return (
+        <>
+            <h1>Our Dogs</h1>
+            <ul>
+                {dogsList.map(dog => (
+                    <DogCardComponent key={dog.id} dog={dog} onMoreInfo={NavigateToDogInfo}></DogCardComponent>
+                   ))}
+
+
+                            {/* Ask David
+                            <div className="dog-background"
+                                style={
+                                    dog.img ? {
+                                        backgroundImage: `url(${dog.img})`
+                                    }
+                                        : {}
+                                }
+                            >{!dog.img && (
+                                <div id="missing-img">
+                                    <p>Photo of {dog.name} will be here soon!</p>
+                                </div>
+                            )}
+
+
+                            </div>
+                            */}
+
+
+             
+            </ul>
+        </>
+    )
 }
 
 
